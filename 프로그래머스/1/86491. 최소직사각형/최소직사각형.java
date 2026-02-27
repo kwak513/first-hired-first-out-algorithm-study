405911a03e4fae3df485fba229a028375e20c8db
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-27 18:29:55 +0900

    [level 1] Title: 최소직사각형, Time: 2.33 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,48 +1,35 @@
-import java.util.*;
-
-/*
-- 명함의 긴 변을 '가로'로, 짧은 변을 '세로'로 통일
-*/
-
 class Solution {
     public int solution(int[][] sizes) {
         
-        // 긴 변 (가로) 담을 리스트
-        ArrayList<Integer> garo = new ArrayList<>();
-        // 짧은 변 (세로) 담을 리스트
-        ArrayList<Integer> sero = new ArrayList<>();
+        // 긴 변 저장용
+        int[] longerLength = new int[sizes.length];
+        // 짧은 변 저장용
+        int[] shorterLength = new int[sizes.length];
         
         
         for(int i = 0; i < sizes.length; i++){
-            // 첫번째 숫자
-            int firstNum = sizes[i][0];
-            // 두번째 숫자
-            int secondNum = sizes[i][1];
+            int width = sizes[i][0];
+            int height = sizes[i][1];
             
-            // 더 큰 숫자를 garo에 넣음
-            if(firstNum > secondNum){
-                garo.add(firstNum);
-                sero.add(secondNum);
-            }
             
-            else{
-                garo.add(secondNum);
-                sero.add(firstNum);
-            }
+            longerLength[i] = Math.max(width, height);
+            shorterLength[i] = Math.min(width, height);
+            
         }
         
-        // 정렬
-        Collections.sort(garo);
-        Collections.sort(sero);
-            
-        // 마지막 인덱스 추출    
-        int garoLength = garo.size();
-        int seroLength = sero.size();
+        int maxOfLongerLength = longerLength[0];
         
-        int finalGaro = garo.get(garoLength - 1);
-        int finalSero = sero.get(seroLength - 1);
+        for(int longL: longerLength){
+            maxOfLongerLength = Math.max(longL, maxOfLongerLength);
+        }
         
-        return finalGaro * finalSero;
+        int maxOfShorterLength = shorterLength[0];
+        
+        for(int shortL: shorterLength){
+            maxOfShorterLength = Math.max(shortL, maxOfShorterLength);
+        }
+        
+        return maxOfLongerLength * maxOfShorterLength;
         
     }
 }
