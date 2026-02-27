4eb1bc46bff7feb6adb9c921a466ce45047f3f38
Chaeyeon Kwak <kwak513@hufs.ac.kr>
2026-02-27 19:36:56 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,42 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         
         
-        for(int i = 1; i <= yellow; i++){
-            if(yellow % i == 0){
-                int j = yellow / i;
+        for(int yellowHeight = 1; yellowHeight <= Math.sqrt(yellow); yellowHeight++){
+            
+            if(yellow % yellowHeight == 0){
+                int yellowWidth = yellow / yellowHeight;
                 
-                if((i + 2) * (j + 2) - (i * j) == brown){
-                    int garo = Math.max(i + 2, j + 2);
-                    int sero = Math.min(i + 2, j + 2);
-                    
-                    return new int[]{garo, sero};
+                if((yellowWidth + 2) * (yellowHeight + 2) == brown + yellow){
+                    return new int[]{yellowWidth + 2, yellowHeight + 2};
                 }
-                
             }
+            
+            
+            
         }
         
-        return new int[1];
+        return new int[]{-1, -1};
+        
+        
+        
         
     }
 }
-
-
-/*
-가로 >= 세로
-
-- 노란색 사각형의 가로 a, 세로 b
-- 갈색 사각형의 가로 (a + 2), 세로 (b + 2)
-
-- 노란색 개수 24
-    - 1*24
-    - 2*12
-    - 3*8
-    - 4*6
-
-
-
-
-*/
